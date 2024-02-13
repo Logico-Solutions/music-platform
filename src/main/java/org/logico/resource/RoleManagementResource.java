2377504ac8a4aba37f70b5b60c0abedbaa29e32a
DolphieDude <andreycherevat@gmail.com>
2024-02-13 02:22:28 +0100

    Added new API responses for /roles

--- src/main/java/org/logico/resource/RoleManagementResource.java
+++ src/main/java/org/logico/resource/RoleManagementResource.java
@@ -1,6 +1,6 @@
 package org.logico.resource;
 
-import jakarta.annotation.security.PermitAll;
+import io.quarkus.security.Authenticated;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
@@ -15,6 +15,7 @@ import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.logico.dto.response.RoleResponseDto;
 import org.logico.mapper.RoleMapper;
@@ -36,10 +37,14 @@ public class RoleManagementResource {
     RoleRepository roleRepository;
 
     @GET
-    @PermitAll
+    @Authenticated
     @Operation(summary = "Getting list of Roles with pagination and sorting")
-    @APIResponse(responseCode = "200", description = Constants.SUCCESSFULLY_RETRIEVED, content = {
-            @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = RoleResponseDto.class))})
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = Constants.SUCCESSFULLY_RETRIEVED, content = {
+                    @Content(mediaType = MediaType.APPLICATION_JSON,
+                            schema = @Schema(implementation = RoleResponseDto.class))}),
+            @APIResponse(responseCode = "401", description = "Unauthorized"),
+            @APIResponse(responseCode = "403", description = "Not authorized to view")})
     public Response getRoles(@QueryParam("page") @DefaultValue("0") int pageIndex,
             @QueryParam("size") @DefaultValue("10") int pageSize) {
         List<RoleResponseDto> roles = roleRepository
