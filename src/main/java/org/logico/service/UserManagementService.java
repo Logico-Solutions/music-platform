38ad3007ad0ccba45ea16040fa44255775425675
Teract10s <trs.leonov@gmail.com>
2024-02-14 11:48:57 +0200

    implemented getting user info by jwt endpoint

---
+++ src/main/java/org/logico/service/UserManagementService.java
@@ -0,0 +1,26 @@
+package org.logico.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.jbosslog.JBossLog;
+import org.logico.dto.response.UserResponseDto;
+import org.logico.mapper.UserMapper;
+import org.logico.model.User;
+import org.logico.repository.UserRepository;
+
+@JBossLog
+@RequiredArgsConstructor
+@ApplicationScoped
+public class UserManagementService {
+
+    private final UserMapper userMapper;
+    private final UserRepository userRepository;
+
+    public UserResponseDto getUserInfoByUsername(String username) {
+        User userFromDB = userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new EntityNotFoundException("Can't find user by username: " + username));
+        return userMapper.toDto(userFromDB);
+    }
+
+}
