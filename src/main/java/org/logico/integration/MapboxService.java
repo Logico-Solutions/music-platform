65bfe0243a70a08fdb0a62753b838fb36d58a063
Oleksii Rozumenko <e477box@gmail.com>
2024-04-10 00:02:34 +0300

    Connection with mapbox api

--- src/main/java/org/logico/integration/MapboxService.java
+++ src/main/java/org/logico/integration/MapboxService.java
@@ -1,5 +1,23 @@
 package org.logico.integration;
 
-public class MapboxService {
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.core.Response;
+import lombok.extern.jbosslog.JBossLog;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.logico.integration.client.MapboxClient;
+import org.logico.service.TrackingUserService;
 
+@JBossLog
+@ApplicationScoped
+public class MapboxService implements MapboxClient {
+
+    MapboxClient mapboxClient;
+    TrackingUserService trackingUserService;
+
+    @Retry(maxRetries = 1, abortOn = {NotFoundException.class})
+    public Response getGeoData(double longitude, double latitude, String accessToken) {
+        log.info("Trying to get geo data from mapbox api");
+        return null;
+    }
 }
--- src/main/java/org/logico/integration/client/MapboxClient.java
+++ src/main/java/org/logico/integration/client/MapboxClient.java
@@ -1,5 +1,21 @@
 package org.logico.integration.client;
 
-public class MapboxClient {
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+@RegisterRestClient(configKey = "mapbox")
+public interface MapboxClient {
+
+    @GET
+    @Path("/api/search/geocode/v6/reverse")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    Response getGeoData(@QueryParam("longitude") double longitude,
+            @QueryParam("latitude") double latitude, @QueryParam("access_token") String accessToken);
 }
